f53197c5d4d41871df448f6913ae1ede6886e40e
Adam <adam@wiedemannlampe.com>
2024-04-21 18:03:01 +0100

    Add filter by country through a filteredData function

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,6 @@
 <script setup>
 const { data } = await useFetch("http://localhost:3500/data.json");
+const selectedCountry = ref("");
 const countries = data.value.reduce((a, d) => {
   if (!a.includes(d.country)) {
     a.push(d.country);
@@ -12,7 +13,23 @@ const regions = data.value.reduce((r, d) => {
   }
   return r;
 }, []);
+
+const filteredData = (country, region) => {
+  const filtered =
+    country || region
+      ? data.value.filter((c) => c.country === country || c.region === region)
+      : data.value;
+  return filtered;
+};
 </script>
 <template>
-  <!-- <Table :data="people" /> -->
+  <div class="section">
+    <InputSelect
+      :name="'Country'"
+      :message="'Filter by country'"
+      :options="countries"
+      v-model="selectedCountry"
+    />
+    <Table :data="filteredData(selectedCountry)" />
+  </div>
 </template>
