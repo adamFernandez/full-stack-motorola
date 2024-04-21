89de7c574b7e8f092b4ec839f2f51631b9ff9287
Adam <adam@wiedemannlampe.com>
2024-04-21 23:41:17 +0100

    Add layout structure and style to app.vue && prettier config

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,66 +1,65 @@
 <script setup>
-const { data } = await useFetch("http://localhost:3500/data.json");
-const selectedCountry = ref("");
+const { data } = await useFetch('http://localhost:3500/data.json')
+const selectedCountry = ref('')
 const countries = data.value.reduce((a, d) => {
   if (!a.includes(d.country)) {
-    a.push(d.country);
+    a.push(d.country)
   }
-  return a;
-}, []);
-const selectedRegion = ref("");
+  return a
+}, [])
+const selectedRegion = ref('')
 const regions = data.value.reduce((r, d) => {
   if (!r.includes(d.region)) {
-    r.push(d.region);
+    r.push(d.region)
   }
-  return r;
-}, []);
+  return r
+}, [])
 
-const filtered = ref(data.value);
+const filtered = ref(data.value)
 const filteredData = (country, region, search) => {
   if (country || region)
     filtered.value = data.value.filter(
       (c) => c.country === country || c.region === region
-    );
+    )
 
   if (searchInput.value)
     filtered.value = data.value.filter(
       (c) =>
         c.name.toLowerCase().includes(search.toLowerCase()) ||
         c.email.toLowerCase().includes(search.toLowerCase())
-    );
+    )
 
-  return filtered.value;
-};
-const searchInput = ref("");
+  return filtered.value
+}
+const searchInput = ref('')
 const find = () => {
   filtered.value = data.value.filter(
     (c) =>
       c.name.toLowerCase().includes(searchInput.value.toLowerCase()) ||
       c.email.toLowerCase().includes(searchInput.value.toLowerCase())
-  );
-};
+  )
+}
 </script>
 <template>
-  <div class="container">
+  <NuxtLayout>
+    <h3 class="heading">Filters:</h3>
+    <!-- <Filters :data="data" :names="['country', 'region']" /> -->
     <div class="section">
-      <h3 class="heading">Filters:</h3>
-      <div class="inline">
-        <InputSelect
-          :name="'Country'"
-          :message="'Filter by country'"
-          :options="countries"
-          v-model="selectedCountry"
-        />
-        <InputSelect
-          :name="'Region'"
-          :message="'Filter by region'"
-          :options="regions"
-          v-model="selectedRegion"
-        />
-      </div>
+      <InputSelect
+        :name="'Country'"
+        :message="'Filter by country'"
+        :options="countries"
+        v-model="selectedCountry"
+      />
+      <InputSelect
+        :name="'Region'"
+        :message="'Filter by region'"
+        :options="regions"
+        v-model="selectedRegion"
+      />
+      <InputField :name="'search'" :label="'Search'" v-model="searchInput" />
     </div>
-  </div>
-  <InputField :name="'search'" :label="'Search'" v-model="searchInput" />
 
-  <Table :data="filteredData(selectedCountry, selectedRegion, searchInput)" />
+    <Table :data="filteredData(selectedCountry, selectedRegion, searchInput)" />
+  </NuxtLayout>
 </template>
---
+++ frontend/layouts/default.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div class="main">
+    <slot />
+  </div>
+</template>
