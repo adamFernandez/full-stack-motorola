e6537caa71a9f86e2d861cb73d7d6e3e71b75e2b
Adam <adam@wiedemannlampe.com>
2024-04-21 10:30:30 +0100

    Add filtered countries select options

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,7 +1,12 @@
 <script setup>
-const { data: people } = await useFetch("http://localhost:3500/data.json");
+const { data } = await useFetch("http://localhost:3500/data.json");
+const countries = data.value.reduce((a, d) => {
+  if (!a.includes(d.country)) {
+    a.push(d.country);
+  }
+  return a;
+}, []);
 </script>
 <template>
-  <Table :data="people" />
+  <!-- <Table :data="people" /> -->
 </template>
-s
