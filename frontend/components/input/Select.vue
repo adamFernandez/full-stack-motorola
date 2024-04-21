0434bddee44bcc0f87c7357f30f671a7bdb02a2a
Adam <adam@wiedemannlampe.com>
2024-04-21 18:50:30 +0100

    Add Input Select component

---
+++ frontend/components/input/Select.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  options: Array,
+  name: String,
+  message: String,
+  error: String,
+  inputState: String,
+});
+const modelValue = ref("default");
+defineEmits(["update:modelValue"]);
+</script>
+<template>
+  <span class="field">
+    <select
+      v-model="modelValue"
+      :name="name"
+      :class="[inputState, 'field__input field__select']"
+      @change="$emit('update:modelValue', $event.target.value)"
+    >
+      <option disabled selected value="default">{{ message }}</option>
+      ]
+      <option value="none">None</option>
+      <option v-for="(option, i) in options">{{ option }}</option>
+    </select>
+    <span :class="['error']">{{ error }}</span>
+  </span>
+</template>
