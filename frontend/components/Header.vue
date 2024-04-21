2be5b80152e378220c9b0cdd34aee35f942bab86
Adam <adam@wiedemannlampe.com>
2024-04-22 00:00:42 +0100

    Add header

---
+++ frontend/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['heading'])
+</script>
+<template>
+  <div class="header">
+    <h2>
+      {{ heading }}
+    </h2>
+  </div>
+</template>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,6 +1,7 @@
 <script setup></script>
 <template>
   <div class="main">
+    <Header :heading="'People Search'" />
     <slot />
   </div>
 </template>
