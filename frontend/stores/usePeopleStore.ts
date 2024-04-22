6fa44ec1659f6403d64999eb1dae8656b47f89f8
Adam <adam@wiedemannlampe.com>
2024-04-22 02:00:37 +0100

    Create people store, state, actions and getters. Some configuration for HMR

---
+++ frontend/stores/usePeopleStore.ts
@@ -0,0 +1,31 @@
+import data from '~/data/data.json'
+
+export const usePeopleStore = defineStore({
+  id: 'people-store',
+  state: () => {
+    return {
+      people: data
+    }
+  },
+
+  actions: {
+    addPerson(country: string, region: string, name: string, email: string) {
+      this.people.push({
+        country,
+        region,
+        name,
+        email
+      })
+    }
+  },
+
+  getters: {
+    getPeople: (state) => {
+      return () => state.people
+    }
+  }
+})
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(usePeopleStore, import.meta.hot))
+}
