2583abd96fe10fb32792aafe643d8d7b0bb7a1d9
Adam <adam@wiedemannlampe.com>
2024-04-22 09:23:11 +0100

    Move Table to section folder, add missing tbody

---
+++ frontend/components/section/Table.vue
@@ -0,0 +1,18 @@
+<script setup>
+const { data } = defineProps(['data'])
+</script>
+<template>
+  <table>
+    <tbody>
+      <tr>
+        <th>Country</th>
+        <th>Region</th>
+        <th>Name</th>
+        <th>Email</th>
+      </tr>
+      <tr v-for="r in data">
+        <td v-for="d in r">{{ d }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
