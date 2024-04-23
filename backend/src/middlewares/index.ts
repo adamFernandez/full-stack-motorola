3a260567df4c50c8afeb57507323157ab4a5c67c
Adam <adam@wiedemannlampe.com>
2024-04-23 22:39:28 +0100

    Add deleteUser controller action & ownership middleware

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -7,6 +7,30 @@ import { getUserBySessionToken } from '../db/users'
 import dotenv from 'dotenv'
 dotenv.config()
 
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params
+    const currentUserId = get(req, 'identity._id') as string
+
+    if (!currentUserId) {
+      return res.sendStatus(403)
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(400)
+    }
+
+    return next()
+  } catch (error) {
+    console.log(error)
+    return res.sendStatus(400)
+  }
+}
+
 export const isAuthenticated = async (
   req: express.Request,
   res: express.Response,
