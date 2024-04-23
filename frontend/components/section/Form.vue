a245b08c8e8f1fd1248b44d1311901d5d537add5
Adam <adam@wiedemannlampe.com>
2024-04-23 17:01:03 +0100

    Set form to dynamic

--- frontend/components/section/Form.vue
+++ frontend/components/section/Form.vue
@@ -1,36 +1,27 @@
 <script setup>
+defineProps(['fields', 'buttonLabel'])
 const peopleStore = usePeopleStore()
 const formData = ref({})
 const resetForm = () => {
   formData.value = {}
 }
+const cap = (string) => {
+  return `${string[0].toUpperCase()}${string.slice(1)}`
+}
 </script>
 <template>
   <form class="form">
     <div class="form__fields">
-      <InputField
-        :name="'country'"
-        :label="'Country'"
-        v-model="formData['country']"
-      />
-      <InputField
-        :name="'region'"
-        :label="'Region'"
-        v-model="formData['region']"
-      />
-      <InputField :name="'name'" :label="'Name'" v-model="formData['name']" />
-      <InputField
-        :name="'email'"
-        :label="'Email'"
-        v-model="formData['email']"
-      />
+      <template v-for="f in fields">
+        <InputField :name="f" :label="cap(f)" v-model="formData[f]" />
+      </template>
     </div>
     <button
       type="submit"
       :class="['button']"
       @click.prevent="peopleStore.addPerson(formData), resetForm()"
     >
-      Add Person
+      {{ buttonLabel }}
     </button>
   </form>
 </template>
