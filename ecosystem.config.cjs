c81a8d8683a59a509982eed72c6ced70c72081eb
Adam <adam@wiedemannlampe.com>
2024-04-26 18:04:41 +0100

    Upate pm2 ecosystem file

---
+++ ecosystem.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      cwd: '/home/ubuntu/full-stack-motorola/frontend',
+      script: './.output/server/index.mjs',
+      interpreter: 'node',
+      args: 'start',
+      name: 'Front Motorola',
+      env: {
+        PORT: 3500,
+        NODE_ENV: 'development'
+      }
+    },
+    {
+      cwd: '/home/ubuntu/full-stack-motorola/backend',
+      script: './dist/index.js',
+      interpreter: 'node',
+      args: 'start',
+      name: 'Back Motorola',
+      env: {
+        PORT: 8080,
+        NODE_ENV: 'development'
+      }
+    }
+  ]
+}
